d3d72779778d7006c3b85c1730e63f00e61a77e6
Kyle Montiel-Berner <kyle_montielberner@outlook.com>
2025-11-24 14:38:01 -0600

    feat: open modal and dialog outline

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ function addBookToLibrary(book) {
 
 function displayBook() {
     for (let i = 0; i < library.length; i++) {
-    let el = document.createElement("div");
+    let el = document.createElement('div');
     el.classList.add("book");
     el.textContent = `Title: ${library[i].title}\n
                     Author: ${library[i].author}\n
@@ -30,8 +30,37 @@ function displayBook() {
     };
 }
 
-
-const theHobbit = new Book('The Hobbit', 'J.R.R. Tolkien', 295, 'not read yet');
-
-addBookToLibrary(theHobbit);
+const theQuietAmerican = new Book('The Quiet American', 'Graham Greene', 240, 'read');
+const theWarofArt = new Book('The War of Art', 'Steven Pressfield', 165, 'read');
+addBookToLibrary(theQuietAmerican);
+addBookToLibrary(theWarofArt);
 displayBook();
+
+/* Add New Book Button and Open Modal Functionality */
+
+const newBookButton = document.createElement('button');
+newBookButton.textContent = "Add New Book";
+header.appendChild(newBookButton);
+
+newBookButton.addEventListener('click', () => {
+    dialog.showModal();
+})
+
+/* Dialog Functionality */
+
+const dialog = document.querySelector('#dialog');
+const form = document.querySelector('form');
+const titleInput = document.querySelector('#title');
+const authorInput = document.querySelector('#author');
+const pagesInput = document.querySelector('#pages');
+const readInput = document.querySelector('#read');
+const addBookBtn = document.querySelector('.add-book-btn');
+const closeBtn = document.querySelector('.close-btn');
+
+const title = document.createElement('div');
+title.textContent = titleInput.value;
+title.appendChild(h)
+
+closeBtn.addEventListener('click', () => {
+    dialog.close();
+});
